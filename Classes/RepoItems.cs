ffe2ad63b386cd9b01c2eba20120475adf6dc508
grigor peshin <linashikhova@mail.ru>
2025-09-29 12:02:29 +0500

    Разработанный класс RepoItems. Казаков

---
+++ Classes/RepoItems.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Kazakov.Classes
+{
+    public class RepoItems
+    {
+        public static List<object> AllItems()
+        {
+            List<object> allItems = new List<object>();
+            allItems.Add(new Children("интерактив", 2200, 3));
+            allItems.Add(new Children("Кактус", 894, 6));
+            allItems.Add(new Children("Кошка подушка", 1754, 6));
+            allItems.Add(new Sport("Споривный костюм", 4891, "S"));
+            allItems.Add(new Sport("Водяной мяч", 512, "61-63 см"));
+            allItems.Add(new Sport("Набор для гольфа", 3950, "600*800 мм"));
+
+            return allItems;
+        }
+    }
+}
