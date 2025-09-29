78a26dae3114296a6ccc55d1bad630628163992c
grigor peshin <linashikhova@mail.ru>
2025-09-29 11:57:55 +0500

    Разработанный класс Children. Казаков

---
+++ Classes/Children.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Kazakov.Classes
+{
+    public class Children:Shop
+    {
+        public int Age { get; set; }
+        public Children(string Name, int Price, int Age) : base(Name, Price) {
+            this.Age = Age;
+        }
+    }
+}
