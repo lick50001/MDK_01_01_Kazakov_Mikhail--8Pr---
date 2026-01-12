4d67c24941848f56decd6e112f17591ea0e6f9a0
ANosok <kirill60278@gmail.com>
2026-01-12 18:12:46 +0500

    Разработанный контекст для детских товаров.  Казаков

---
+++ Classes/ChildrenContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop_Kazakov.Interfaces;
+using Shop_Kazakov.Models;
+
+namespace Shop_Kazakov.Classes
+{
+    public class ChildrenContext: ChildrenContext, IContext
+    {
+        public ChildrenContext() { }
+        public ChildrenContext(int id, string Name, int Price, int Age, int IdShop)
+        {
+
+        }
+
+        public List<object> All()
+        {
+            List<object> allShop = new List<object>();
+            List<object> allChildren = new List<object>();
+            OleDbConnection conn = Common.DBConnection.Connection();
+            OleDbDataReader childrenData = Common.DBConnection.Query("Select * From [Детские вещи]", conn);
+            while (childrenData.Read() )
+            {
+                ShopContext ShopElement = allShop.Find(x => (x as ShopContext).Id == childrenData.GetInt32(2)) as ShopContext;
+                ChildrenContext newChildren = new ChildrenContext(
+                    ShopElement.Id,
+                    ShopElement.Name,
+                    ShopElement.Price,
+                    childrenData.GetInt32(1),
+                    childrenData.GetInt32(2)
+                    );
+                allChildren.Add(newChildren);
+            }
+            Common.DBConnection.CloseConnection(conn);
+
+            return allChildren;
+        }
+
+        public void Save(bool Update = false)
+        {
+
+        }
+    }
+}
--- Models/Children.cs
+++ Models/Children.cs
@@ -9,8 +9,10 @@ namespace Shop_Kazakov.Models
     public class Children:Shop
     {
         public int Age { get; set; }
-        public Children(string Name, int Price, int Age) : base(Name, Price) {
+        public int IdShop { get; set; }
+        public Children(string Name, int Price, int Age, int IdShop) : base(Id, Name, Price) {
             this.Age = Age;
+            this.IdShop = IdShop;
         }
     }
 }
