de8444b6aa311d840925b71a5524e4c7e103575d
ANosok <kirill60278@gmail.com>
2026-01-12 17:51:00 +0500

    Разработка общего класса.  Казаков

---
+++ Classes/Common/DBConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Kazakov.Classes.Common
+{
+    public class DBConnection
+    {
+        public static readonly string Path = @"";
+
+        public static OleDbConnection Conn()
+        {
+            OleDbConnection oleConn = new OleDbConnection("Provider=Microsoft.Ace.Oledb.12.0; Data Sourse=" + Path);
+            oleConn.Open();
+
+            return oleConn;
+        }
+
+        public static OleDbDataReader Query(string Query, OleDbConnection conn) { 
+            // Создать команду
+            return new OleDbCommand(Query, conn).ExecuteReader();    
+        }
+
+        public static void CloseConnection(OleDbConnection conn)
+        {
+            conn.Close();
+        }
+    }
+}
