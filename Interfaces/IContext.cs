8646d284829f973998ff745312720326cb74fa5d
ANosok <kirill60278@gmail.com>
2026-01-12 17:45:24 +0500

    Разработанные интерфейсы. Казаков

---
+++ Interfaces/IContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Kazakov.Interfaces
+{
+    public interface IContext
+    {
+        List<object> All();
+        void Save(bool Update = false);
+        void Delete();
+    }
+}
